951608328fa5c7b6a03f8285bf3fc8a29a542a17
hjk <joo6077@gmail.com>
2020-11-14 00:20:34 +0900

    axios install and callback

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -3,6 +3,8 @@ import Vue from 'vue'
 import NewsView from '../views/NewsView.vue'
 import AskView from '../views/AskView.vue'
 import JobsView from '../views/JobsView.vue'
+import UserView from '../views/UserView.vue'
+import ItemView from '../views/ItemView.vue'
 
 
 Vue.use(VueRouter);
@@ -29,13 +31,13 @@ export const router = new VueRouter({
             path: '/jobs',
             component: JobsView,
         },
-        {
-            path: '/user',
-            component: UserView,
-        },
-        {
-            path: '/item',
-            component: ItemView,
-        }
+        // {
+        //     path: '/user',
+        //     component: UserView,
+        // },
+        // {
+        //     path: '/item',
+        //     component: ItemView,
+        // }
     ]
 })
