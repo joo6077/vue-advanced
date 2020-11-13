fe581b4d702a0c0f8a66700a3cc2c67dead58f21
hjk <joo6077@gmail.com>
2020-11-13 23:50:35 +0900

    Toolbar css

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div id="app">
+    <!-- strongly recommended -->
+    <!-- <ToolBar></ToolBar> -->
+    <!-- essential -->
+    <tool-bar></tool-bar>
+    
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
-import HelloWorld from './components/HelloWorld.vue'
-
+import ToolBar from './components/ToolBar.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ToolBar,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  padding: 0;
+  margin: 0;
 }
 </style>
